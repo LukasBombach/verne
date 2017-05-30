2546375d0fa7afef92fa8e76724f23dd834ebd7f
Lukas Bombach <lukas.bombach@googlemail.com>
2017-05-30 20:14:02 +0200

    Set up webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: {
+    index: ['./src/index.js']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'write-js',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  target: 'node',
+  externals: [nodeExternals()]
+};

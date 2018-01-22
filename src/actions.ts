6f58077522b7f2b9382980ae86410c69a1a1a575
Lukas Bombach <lukas.bombach@googlemail.com>
2018-01-22 23:12:33 +0100

    middleware heavy lifting

--- src/actions.ts
+++ src/actions.ts
@@ -7,32 +7,62 @@ interface ActionHandlers {
 export default class Actions {
 
   private editor: WriteEditor;
-  private actionHandlers: ActionHandlers;
+  private actionHandlers: ActionHandlers = {};
+  private middlewares: Function[] = [];
 
   constructor(editor: WriteEditor) {
     this.editor = editor;
-    this.actionHandlers = {};
+
+    this.middlewares = [
+      (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving a');       next('a'); },
+      (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving b');       next('b'); },
+      (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving c');       next('c'); },
+      (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving d', next); next('d'); },
+    ];
+
+    const composedMiddlewares = this.composeMiddlewares();
+
+    const result = composedMiddlewares(
+      (action: any) => { console.log('got', action, 'terminating'); }
+    );
+
+    console.log('result(\'action\')', result('action'));
   }
 
-  async dispatch(action: any): Promise<Actions> {
-    console.log(action);
-    // todo middleware
-    await this.handle(action);
+  public async dispatch(action: any): Promise<Actions> {
+    // console.log(action);
+    // const actionAfterMiddlewares = await this.composeMiddlewares(action);
+    // await this.handle(actionAfterMiddlewares);
     return this;
   }
 
-  async handle(action: any): Promise<Actions> {
+  public registerActionHandler(actionName: string, handler: Function): Actions {
+    this.actionHandlers[actionName] = handler;
+    return this;
+  }
+
+  private composeMiddlewares(): Function {
+    const chain = this.middlewares.map(middleware => middleware(this.editor));
+    return chain.reduce((a, b) => (...args: any[]) => a(b(...args)));
+  }
+
+  // private composeMiddlewares(middlewares: Function[]): Function {
+  //   if (middlewares.length === 0) {
+  //     return (arg: any) => arg
+  //   }
+  //   if (middlewares.length === 1) {
+  //     return middlewares[0]
+  //   }
+  //   return middlewares.reduce((a, b) => (...args: any[]) => a(b(...args)))
+  // }
+
+  private async handle(action: any): Promise<Actions> {
     const handler = this.actionHandlers[action.type];
     if (handler) {
       await handler(action);
     } else {
       console.warn(`No handler or action ${action.type}`);
     }
-    return this;
-  }
-
-  registerActionHandler(actionName: string, handler: Function): Actions {
-    this.actionHandlers[actionName] = handler;
     return this;
   }
 

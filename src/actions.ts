5f8744ecbe3b43f2abd838bb9bfb4f5bf444e5f6
Lukas Bombach <lukas.bombach@googlemail.com>
2018-01-22 23:45:45 +0100

    integrated middlewares

--- src/actions.ts
+++ src/actions.ts
@@ -9,30 +9,22 @@ export default class Actions {
   private editor: WriteEditor;
   private actionHandlers: ActionHandlers = {};
   private middlewares: Function[] = [];
+  private composedMiddlewares: Function;
 
   constructor(editor: WriteEditor) {
     this.editor = editor;
-
     this.middlewares = [
       (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving a');       next('a'); },
       (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving b');       next('b'); },
       (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving c');       next('c'); },
       (editor: WriteEditor) => (next: Function) => (action: any) => { console.log('got', action, 'giving d', next); next('d'); },
     ];
-
-    const composedMiddlewares = this.composeMiddlewares();
-
-    const result = composedMiddlewares(
-      (action: any) => { console.log('got', action, 'terminating'); }
-    );
-
-    console.log('result(\'action\')', result('action'));
+    this.composedMiddlewares = this.composeMiddlewares(this.middlewares);
   }
 
   public async dispatch(action: any): Promise<Actions> {
-    // console.log(action);
-    // const actionAfterMiddlewares = await this.composeMiddlewares(action);
-    // await this.handle(actionAfterMiddlewares);
+    const actionAfterMiddlewares = await this.applyMiddlewares(action);
+    await this.handle(actionAfterMiddlewares);
     return this;
   }
 
@@ -41,25 +33,26 @@ export default class Actions {
     return this;
   }
 
-  private composeMiddlewares(): Function {
-    const chain = this.middlewares.map(middleware => middleware(this.editor));
-    return chain.reduce((a, b) => (...args: any[]) => a(b(...args)));
+  private applyMiddlewares(action: any): Promise<any> {
+    return new Promise(resolve => this.composedMiddlewares((action: any) => resolve(action))(action))
   }
 
-  // private composeMiddlewares(middlewares: Function[]): Function {
-  //   if (middlewares.length === 0) {
-  //     return (arg: any) => arg
-  //   }
-  //   if (middlewares.length === 1) {
-  //     return middlewares[0]
-  //   }
-  //   return middlewares.reduce((a, b) => (...args: any[]) => a(b(...args)))
-  // }
+  private composeMiddlewares(middlewares: Function[]): Function {
+    if (middlewares.length === 0) {
+      return (arg: any) => arg;
+    }
+    if (middlewares.length === 1) {
+      return middlewares[0];
+    }
+    return middlewares
+      .map(middleware => middleware(this.editor))
+      .reduce((a, b) => (...args: any[]) => a(b(...args)));
+  }
 
-  private async handle(action: any): Promise<Actions> {
+  private handle(action: any): Actions {
     const handler = this.actionHandlers[action.type];
     if (handler) {
-      await handler(action);
+      handler(action);
     } else {
       console.warn(`No handler or action ${action.type}`);
     }

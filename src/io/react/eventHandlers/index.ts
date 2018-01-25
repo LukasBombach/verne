2d4ea506563cf7147466c20b0058894ff6289a1a
Lukas Bombach <lukas.bombach@googlemail.com>
2018-01-25 10:55:28 +0100

    documentation

--- src/io/react/eventHandlers/index.ts
+++ src/io/react/eventHandlers/index.ts
@@ -5,6 +5,23 @@ const eventHandlers: [string, Function][] = [
   ['onKeyDown', handleKeyDown]
 ];
 
+/**
+ * returns an object that can be set as props to the react editor's content editable element
+ * [
+ *   ['onKeyDown', handleKeyDown],
+ *   ['onClick', handleClick]
+ * ]
+ *
+ * becomes
+ *
+ * {
+ *   onKeyDown: (event) => handleKeyDown(eventHandlerInterface, event),
+ *   onClick: (event) => handleClick(eventHandlerInterface, event)
+ * }
+ *
+ * @param {EventHandlerInterface} eventHandlerInterface
+ * @returns {Object}
+ */
 export default function getEventHandlers(eventHandlerInterface: EventHandlerInterface) {
   return eventHandlers
     .map(([eventName, handler]) => [eventName, (e: any): any => handler(eventHandlerInterface, e)])

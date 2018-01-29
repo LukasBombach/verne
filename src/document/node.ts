99e01e6c21f3956b94e06db5f917d25c3dc1334e
Lukas Bombach <lukas.bombach@googlemail.com>
2018-01-29 11:02:53 +0100

    refactoring

--- src/document/node.ts
+++ src/document/node.ts
@@ -30,76 +30,77 @@ export default class Node {
 
   // ✅
   prev(condition = (node: Node) => true): Node {
-    const sibling = this.prevSiblings().reverse().find(sibling => condition(sibling));
+    const sibling = this.prevSiblings().reverse().find(condition);
     return sibling || null;
   }
 
   // ✅
   next(condition = (node: Node) => true): Node {
-    const sibling = this.nextSiblings().find(sibling => condition(sibling));
+    const sibling = this.nextSiblings().find(condition);
     return sibling || null;
   }
 
   // ✅
   firstSibling(condition = (node: Node) => true): Node {
-    const sibling = this.siblings().find(sibling => condition(sibling));
+    const sibling = this.siblings().find(condition);
     return sibling || null;
   }
 
   // ✅
   lastSibling(condition = (node: Node) => true): Node {
-    const sibling = this.siblings().reverse().find(sibling => condition(sibling));
+    const sibling = this.siblings().reverse().find(condition);
     return sibling || null;
   }
 
-  prevLeaf(condition?: (parent: Node) => boolean): Node {
-    const lastLeaf = this.prevSiblings().reduceRight((pre, cur) => pre || cur.lastLeaf(condition), null);
-    if (lastLeaf) return lastLeaf;
-    const parentWithPrev = this.parent(parent => !!parent.prev());
-    if (parentWithPrev) return parentWithPrev.prevLeaf(condition);
-    return null;
-  }
+  // prevLeaf(condition = (node: Node) => true): Node {
+  // prevLeaf(condition?: (parent: Node) => boolean): Node {
+  //   const lastLeaf = this.prevSiblings().reduceRight((pre, cur) => pre || cur.lastLeaf(condition), null);
+  //   if (lastLeaf) return lastLeaf;
+  //   const parentWithPrev = this.parent(parent => !!parent.prev());
+  //   if (parentWithPrev) return parentWithPrev.prevLeaf(condition);
+  //   return null;
+  // }
 
-  nextLeaf(condition?: (parent: Node) => boolean): Node {
-    // const next = this.next();
-    // if (next) return next.firstLeaf();
-    // const parentWithNext = this.parent(parent => !!parent.next());
-    // if (parentWithNext) return parentWithNext.nextLeaf();
-    // return null;
-  }
+  // nextLeaf(condition?: (parent: Node) => boolean): Node {
+  //   const next = this.next();
+  //   if (next) return next.firstLeaf();
+  //   const parentWithNext = this.parent(parent => !!parent.next());
+  //   if (parentWithNext) return parentWithNext.nextLeaf();
+  //   return null;
+  // }
 
-  firstLeaf(): Node {
-    // const children = this.children();
-    // if (!children.length) return this;
-    // return children[0].firstLeaf();
-  }
+  // firstLeaf(): Node {
+  //   const children = this.children();
+  //   if (!children.length) return this;
+  //   return children[0].firstLeaf();
+  // }
 
-  lastLeaf(condition = (node: Node) => true): Node {
-    const children = this.children();
-    if (!children.length) return condition(this) ? this : null;
-    const lastSibling = this.lastSibling(condition);
-    return lastSibling ? lastSibling.lastLeaf() : null;
-  }
+  // lastLeaf(condition = (node: Node) => true): Node {
+  //   const children = this.children();
+  //   if (!children.length) return condition(this) ? this : null;
+  //   const lastSibling = this.lastSibling(condition);
+  //   return lastSibling ? lastSibling.lastLeaf() : null;
+  // }
 
   // ✅
-  siblings(): Node[] {
+  siblings(condition = (node: Node) => true): Node[] {
     const parent = this.parent();
-    return parent ? parent.children() : [this];
+    return parent ? parent.children(condition) : condition(this) ? [this] : [];
   }
 
   // ✅
-  prevSiblings(): Node[] {
-    return this.siblings().slice(0, this.index());
+  prevSiblings(condition = (node: Node) => true): Node[] {
+    return this.siblings(condition).slice(0, this.index());
   }
 
   // ✅
-  nextSiblings(): Node[] {
-    return this.siblings().slice(this.index() + 1);
+  nextSiblings(condition = (node: Node) => true): Node[] {
+    return this.siblings(condition).slice(this.index() + 1);
   }
 
   // ✅
-  children(): Node[] {
-    return this._children;
+  children(condition = (node: Node) => true): Node[] {
+    return this._children.filter(condition);
   }
 
   // ✅

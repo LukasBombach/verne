3b429e9a2136cb235a040b6699baf4a83e5a1723
Lukas Bombach <lukas.bombach@googlemail.com>
2018-02-04 22:24:33 +0100

    forgot to add file in last commit

---
+++ src/io/react/eventHandlers/onBeforeInput.ts
@@ -0,0 +1,12 @@
+import {KeyboardEvent} from "react";
+import Selection from '../selection';
+import Editor from "../components/editor";
+
+export default async function handleBeforeInput(editor: Editor, e: any) {
+  e.preventDefault();
+  const selectionJson = Selection.getUserSelection().toJson();
+  const action = { type: 'input', selection: selectionJson, str: e.data };
+  const { doc, selection } = await editor.core.actions.dispatch(action);
+  editor.setState(() => ({ nodes: doc.children() }));
+  if (selection) Selection.setCaret(selection.focusNode, selection.focusOffset);
+}

202739ce15e2d48aac5949f2d87d4e1fe06a7939
Lukas Bombach <lukas.bombach@googlemail.com>
2018-02-09 16:39:42 +0100

    dynamically typed node class (and added content property)

--- src/document/node.ts
+++ src/document/node.ts
@@ -1,19 +1,20 @@
 interface CloneProperties {
-  parent?: Node;
-  children?: Node[];
+  parent?: Node<any>;
+  children?: Node<any>[];
   originId?: number;
 }
 
-export default class Node {
+export default class Node<C> {
 
   private static nextNodeId = 0;
 
   private _id: number;
   private _originId: number;
-  private _parent: Node;
-  private _children: Node[];
+  private _parent: Node<any>;
+  private _children: Node<any>[];
+  private _content: C;
 
-  constructor(parent: Node = null, children: Node[] = [], originId: number = null) {
+  constructor(parent: Node<any> = null, children: Node<any>[] = [], originId: number = null) {
     this._id = ++Node.nextNodeId;
     this._originId = originId || this._id;
     this._parent = parent;
@@ -28,45 +29,49 @@ export default class Node {
     return this._originId;
   }
 
+  get content(): C {
+    return this._content;
+  }
+
   get index(): number {
     return this.parent() ? this.siblings().indexOf(this) : 0;
   }
 
-  get path(): Node[] {
+  get path(): Node<any>[] {
     const parent = this.parent();
     return parent ? [this, ...parent.path] : [this];
   }
 
-  children(condition = (node: Node) => true): Node[] {
+  children(condition = (node: Node<any>) => true): Node<any>[] {
     return this._children ? this._children.filter(condition) : [];
   }
 
-  parent(condition = (node: Node) => true): Node {
+  parent(condition = (node: Node<any>) => true): Node<any> {
     if (condition(this._parent)) return this._parent;
     return this._parent ? this._parent.parent(condition) : null;
   }
 
-  prev(condition = (node: Node) => true): Node {
+  prev(condition = (node: Node<any>) => true): Node<any> {
     const sibling = this.prevSiblings().reverse().find(condition);
     return sibling || null;
   }
 
-  next(condition = (node: Node) => true): Node {
+  next(condition = (node: Node<any>) => true): Node<any> {
     const sibling = this.nextSiblings().find(condition);
     return sibling || null;
   }
 
-  firstSibling(condition = (node: Node) => true): Node {
+  firstSibling(condition = (node: Node<any>) => true): Node<any> {
     const sibling = this.siblings().find(condition);
     return sibling || null;
   }
 
-  lastSibling(condition = (node: Node) => true): Node {
+  lastSibling(condition = (node: Node<any>) => true): Node<any> {
     const sibling = this.siblings().reverse().find(condition);
     return sibling || null;
   }
 
-  prevLeaf(condition = (node: Node) => true): Node {
+  prevLeaf(condition = (node: Node<any>) => true): Node<any> {
     const lastLeafInPrev = this.prevSiblings().reduceRight((pre, cur) => pre || cur.lastLeaf(condition), null);
     if (lastLeafInPrev) return lastLeafInPrev;
     const parentWithPrev = this.parent(parent => !!parent && !!parent.prev());
@@ -74,7 +79,7 @@ export default class Node {
     return null;
   }
 
-  nextLeaf(condition = (node: Node) => true): Node {
+  nextLeaf(condition = (node: Node<any>) => true): Node<any> {
     const firstLeafInNext = this.nextSiblings().reduce((pre, cur) => pre || cur.firstLeaf(condition), null);
     if (firstLeafInNext) return firstLeafInNext;
     const parentWithNext = this.parent(parent => !!parent && !!parent.next());
@@ -82,63 +87,63 @@ export default class Node {
     return null;
   }
 
-  firstLeaf(condition = (node: Node) => true): Node {
+  firstLeaf(condition = (node: Node<any>) => true): Node<any> {
     const children = this.children();
     if (!children.length) return condition(this) ? this : null;
     const firstLeaf = children.reduce((pre, cur) => pre || cur.firstLeaf(condition), null);
     return firstLeaf || null;
   }
 
-  lastLeaf(condition = (node: Node) => true): Node {
+  lastLeaf(condition = (node: Node<any>) => true): Node<any> {
     const children = this.children();
     if (!children.length) return condition(this) ? this : null;
     const lastLeaf = children.reduceRight((pre, cur) => pre || cur.lastLeaf(condition), null);
     return lastLeaf || null;
   }
 
-  siblings(condition = (node: Node) => true): Node[] {
+  siblings(condition = (node: Node<any>) => true): Node<any>[] {
     const parent = this.parent();
     return parent ? parent.children(condition) : condition(this) ? [this] : [];
   }
 
-  prevSiblings(condition = (node: Node) => true): Node[] {
+  prevSiblings(condition = (node: Node<any>) => true): Node<any>[] {
     return this.siblings(condition).slice(0, this.index);
   }
 
-  nextSiblings(condition = (node: Node) => true): Node[] {
+  nextSiblings(condition = (node: Node<any>) => true): Node<any>[] {
     return this.siblings(condition).slice(this.index + 1);
   }
 
-  nextSiblingsUntil(condition = (node: Node) => false): Node[] {
+  nextSiblingsUntil(condition = (node: Node<any>) => false): Node<any>[] {
     const nextSiblings = this.nextSiblings();
     return nextSiblings.slice(0, nextSiblings.findIndex(condition));
   }
 
-  pathUntil(condition = (node: Node) => true): Node[] {
+  pathUntil(condition = (node: Node<any>) => true): Node<any>[] {
     const parent = this.parent(condition);
     return parent ? [this, ...parent.path] : [this];
   }
 
-  comparePositionWith(that: Node): number {
+  comparePositionWith(that: Node<any>): number {
     if (this === that) return 0;
     const [thisIndex, thatIndex] = Node.closestParents(this, that).map(node => node.index);
     return thisIndex < thatIndex ? -1 : thisIndex === thatIndex ? 0 : 1;
   }
 
-  replaceNode(oldNode: Node, newNode: Node): this {
+  replaceNode(oldNode: Node<any>, newNode: Node<any>): this {
       return this.replaceNodes([oldNode], [newNode]);
   }
 
-  deleteNode(nodeToRemove: Node): this {
+  deleteNode(nodeToRemove: Node<any>): this {
       return this.deleteNodes([nodeToRemove]);
   }
 
-  replaceNodes(oldNodes: Node[], newNodes: Node[]): this {
+  replaceNodes(oldNodes: Node<any>[], newNodes: Node<any>[]): this {
     return this;
   }
 
-  deleteNodes(nodesToRemove: Node[]): this {
-    const [oldParents, newParents]: [Node[], Node[]] = [[], []];
+  deleteNodes(nodesToRemove: Node<any>[]): this {
+    const [oldParents, newParents]: [Node<any>[], Node<any>[]] = [[], []];
     Node.groupNodesByParent(nodesToRemove).forEach((children, parent) => {
       oldParents.push(parent);
       newParents.push(parent.deleteChildren(children));
@@ -153,17 +158,17 @@ export default class Node {
     return new (Node as any)(parent, children, originId);
   }
 
-  private replaceChildren(oldChildren: Node[], newChildren: Node[]): this {
+  private replaceChildren(oldChildren: Node<any>[], newChildren: Node<any>[]): this {
     const children = this.children().map(a => newChildren[oldChildren.findIndex(b => b.originId === a.originId)] || a); // todo WIP!!! using the originId is probably the worst idea ever || please come back to // const children = this.children().map(child => newChildren[oldChildren.indexOf(child)] || child);
     return this.clone({ children });
   }
 
-  private deleteChildren(childrenToRemove: Node[]): this {
+  private deleteChildren(childrenToRemove: Node<any>[]): this {
     const children = this.children().filter(a => childrenToRemove.findIndex(b => b.originId === a.originId) === -1); // todo WIP!!! using the originId is probably the worst idea ever || please come back to // const children = this.children().filter(child => childrenToRemove.indexOf(child) === -1);
     return this.clone({ children });
   }
 
-  static nodesBetween(firstNode: Node, lastNode: Node): Node[] {
+  static nodesBetween(firstNode: Node<any>, lastNode: Node<any>): Node<any>[] {
     if (firstNode.parent() === lastNode.parent()) return firstNode.nextSiblingsUntil(node => node === lastNode);
     const [firstParent, lastParent] = Node.closestParents(firstNode, lastNode);
     const firstParentSiblings = firstNode
@@ -178,15 +183,15 @@ export default class Node {
     return [...firstParentSiblings, ...nodesBetweenParents, ...lastParentSiblings];
   }
 
-  private static closestParents(a: Node, b: Node): [Node, Node] {
+  private static closestParents(a: Node<any>, b: Node<any>): [Node<any>, Node<any>] {
     const pathA = a.path;
     const pathB = b.path;
     const index = pathA.findIndex((node, index) => node === pathB[index]);
     return [pathA[index - 1], pathB[index - 1]];
   }
 
-  private static groupNodesByParent(nodes: Node[]): Map<Node, Node[]> {
-    const map = new Map<Node, Node[]>();
+  private static groupNodesByParent(nodes: Node<any>[]): Map<Node<any>, Node<any>[]> {
+    const map = new Map<Node<any>, Node<any>[]>();
     const tuples = nodes.map(node => [node.parent(), node]);
     tuples.forEach(([parent]) => map.set(parent, []));
     tuples.forEach(([parent, node]) => map.get(parent).push(node));

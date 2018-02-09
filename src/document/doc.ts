3888c87927536f4071effdb54529c9d848c78b7b
Lukas Bombach <lukas.bombach@googlemail.com>
2018-02-10 00:57:40 +0100

    Doc api

--- src/document/doc.ts
+++ src/document/doc.ts
@@ -9,15 +9,28 @@ export default class Doc {
     this.nodeMap = nodeMap;
   }
 
-  replaceNodes(oldNodes: Node[], newNodes: Node[]): Doc {
+  getChildren(node: Node): Node[] {
+    const entry = this.nodeMap.get(node);
+    return entry ? entry.children : null;
+  }
+
+  replaceNode(node: Node, newNode: Node): Doc {
+    return this.replaceNodes([node], [newNode]);
+  }
+
+  deleteNode(node: Node): Doc {
+    return this.deleteNodes([node])
+  }
+
+  replaceNodes(nodes: Node[], newNodes: Node[]): Doc {
     const nodeMap = this.nodeMap.clone();
-    oldNodes.forEach((oldNode, index) => nodeMap.replace(oldNode, newNodes[index]));
+    nodes.forEach((node, index) => nodeMap.replace(node, newNodes[index]));
     return new Doc(nodeMap);
   }
 
-  deleteNodes(nodesToRemove: Node[]): Doc {
+  deleteNodes(nodes: Node[]): Doc {
     const nodeMap = this.nodeMap.clone();
-    nodesToRemove.forEach(node => nodeMap.remove(node));
+    nodes.forEach(node => nodeMap.remove(node));
     return new Doc(nodeMap);
   }
 

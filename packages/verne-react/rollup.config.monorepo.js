dc6bb17f9b154ec317af7654458113ff519d1d20
Lukas Bombach <lukas.bombach@googlemail.com>
2018-02-25 13:04:59 +0100

    fixed rollup errors

--- packages/verne-react/rollup.config.monorepo.js
+++ packages/verne-react/rollup.config.monorepo.js
@@ -6,12 +6,16 @@ module.exports = [
   {
     input: path.join('packages', 'verne', 'src', 'index.ts'),
     output: [
-      { file: path.join('packages', 'verne', pkg.main), format: 'cjs' },
-      { file: path.join('packages', 'verne', pkg.browser), format: 'umd', name: 'Verne' },
-      { file: path.join('packages', 'verne', pkg.module), format: 'es' },
+      { exports: 'named', file: path.join('packages', 'verne', pkg.main), format: 'cjs' },
+      { exports: 'named', file: path.join('packages', 'verne', pkg.browser), format: 'umd', name: 'Verne' },
+      { exports: 'named', file: path.join('packages', 'verne', pkg.module), format: 'es' },
     ],
     plugins: [
       typescript(),
     ],
+    onwarn: ( warning, next ) => {
+      if ( warning.code === 'THIS_IS_UNDEFINED' ) return; // you can do this now btw
+      next( warning );
+    },
   },
 ];

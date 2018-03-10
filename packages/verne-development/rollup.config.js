b8021ec1bd77aa303dfed67b299cc65d27c42f97
Lukas Bombach <lukas.bombach@googlemail.com>
2018-03-10 17:00:33 +0100

    added development packages

--- packages/verne-development/rollup.config.js
+++ packages/verne-development/rollup.config.js
@@ -1,7 +1,8 @@
 import path from 'path';
+import typescript  from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import typescript  from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
 import pkg  from './package.json';
 
 const basePath = process.env.USE_VERNE_PACKAGES ? path.join('packages', 'verne') : '';
@@ -10,33 +11,25 @@ export default [
   {
     input: path.join(basePath, 'src', 'index.ts'),
     output: {
-      file: path.join(basePath, pkg.browser),
+      file: path.join(basePath, pkg.main),
       format: 'umd',
-      name: 'Verne',
-      exports: 'named',
     },
     plugins: [
       resolve(),
       commonjs(),
       typescript({ typescript: require('typescript') }),
+      serve({
+        open: true,
+        verbose: true,
+        contentBase: ['lib', 'public'],
+        historyApiFallback: false,
+        host: 'localhost',
+        port: 3000,
+      }),
     ],
     onwarn: (warning, next) => {
       if (warning.code === 'THIS_IS_UNDEFINED') return;
       next(warning);
     },
-  },
-  {
-    input: path.join(basePath, 'src', 'index.ts'),
-    output: [
-      { exports: 'named', file: path.join(basePath, pkg.main), format: 'cjs' },
-      { exports: 'named', file: path.join(basePath, pkg.module), format: 'es' },
-    ],
-    plugins: [
-      typescript({ typescript: require('typescript') }),
-    ],
-    onwarn: (warning, next) => {
-      if (warning.code === 'THIS_IS_UNDEFINED') return;
-      next(warning);
-    },
-  },
+  }
 ];

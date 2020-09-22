711cb8b0958e1596934e6138ed3c0c8665e43cb7
Lukas Bombach <lukas.bombach@googlemail.com>
2020-09-22 08:54:14 +0200

    implemented useCaret

--- verne3/src/components/Verne2.tsx
+++ verne3/src/components/Verne2.tsx
@@ -1,13 +1,99 @@
 import React, { useState, useRef, useEffect } from "react";
+import mitt from "mitt";
 
-const editableProps = {
-  contentEditable: true,
-  spellCheck: false,
-  suppressContentEditableWarning: true,
-  style: { whiteSpace: "pre-wrap" },
-} as const;
+// import type { Handler } from "mitt";
 
-const getOffset = () => {
+const emitter = mitt();
+
+const Text = ({ text }: { text: string }) => {
+  const { onKeyDown, insertText, caret } = useVerne();
+  onKeyDown(key => insertText(caret.offset, key));
+  return <span>{text}</span>;
+};
+
+Text.onKeyDown = (key: string, { text }: { text: string }) => {};
+
+const Verne = () => {
+  const [text, setText] = useState("hello world");
+  const editorRef = useRef<HTMLDivElement>(null);
+  const caretRef = useRef<{
+    node: ChildNode;
+    offset: number;
+  }>();
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    if (!/^[\w\s]$/.test(event.key)) return;
+    const offset = getOffset();
+    const newText = insertString(text, offset, event.key);
+    setText(newText);
+    const node = editorRef.current?.firstChild?.firstChild;
+    if (node) caretRef.current = { node, offset };
+  };
+
+  useEffect(() => {
+    if (caretRef.current)
+      setCaret(caretRef.current.node, caretRef.current.offset + 1);
+  });
+
+  return (
+    <div {...getEditableProps()} onKeyDown={onKeyDown} ref={editorRef}>
+      <Text text={text} />
+      <Text text=" text2" />
+    </div>
+  );
+};
+
+interface Caret {
+  node: Node;
+  offset: number;
+}
+
+function useCaret() {
+  const caretRef = useRef<Caret>();
+  const setCaret = (caret: Caret) => (caretRef.current = caret);
+  const caret = caretRef.current;
+
+  useEffect(() => {
+    if (caret) {
+      const range = new Range();
+      range.setStart(caret.node, caret.offset);
+      range.setEnd(caret.node, caret.offset);
+      window.document.getSelection()?.removeAllRanges();
+      window.document.getSelection()?.addRange(range);
+    }
+  });
+
+  return [caret, setCaret];
+}
+
+function useVerne() {
+  function onKeyDown(handler: (key: string) => void) {
+    useEffect(() => {
+      console.log("onKeyDown called");
+    }, []);
+  }
+
+  function insertText(offset: number, text: string) {}
+
+  const caret = {
+    get offset() {
+      return 1;
+    },
+  };
+
+  return { onKeyDown, insertText, caret };
+}
+
+function useInput() {
+  useEffect(() => {
+    console.log("useInput in useEffect");
+  }, []);
+}
+
+function useEvents() {}
+
+function getOffset() {
   const selection = window.document.getSelection();
   const range = selection?.getRangeAt(0);
   const offset = range?.startOffset;
@@ -15,7 +101,7 @@ const getOffset = () => {
     throw new Error("Cannot get offset");
   }
   return offset;
-};
+}
 
 function setCaret(node: Node, offset: number) {
   const range = new Range();
@@ -29,37 +115,27 @@ function insertString(text: string, offset: number, stringToInsert: string) {
   return text.slice(0, offset) + stringToInsert + text.slice(offset);
 }
 
-const Text = ({ text }: { text: string }) => <span>{text}</span>;
-
-const Verne = () => {
-  const [text, setText] = useState("hello world");
-  const editorRef = useRef<HTMLDivElement>(null);
-  const caretRef = useRef<{
-    node: ChildNode;
-    offset: number;
-  }>();
-
-  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    const offset = getOffset();
-    const newText = insertString(text, offset, event.key);
-    setText(newText);
-    const node = editorRef.current?.firstChild?.firstChild;
-    if (node) caretRef.current = { node, offset };
-    console.log(node?.textContent);
-  };
-
-  useEffect(() => {
-    console.log("ref was updated");
-    if (caretRef.current)
-      setCaret(caretRef.current.node, caretRef.current.offset + 1);
-  }, [text]);
-
-  return (
-    <div {...editableProps} onKeyDown={onKeyDown} ref={editorRef}>
-      <Text text={text} />
-    </div>
-  );
-};
+function getEditableProps() {
+  return {
+    contentEditable: true,
+    spellCheck: false,
+    suppressContentEditableWarning: true,
+    style: { whiteSpace: "pre-wrap" },
+  } as const;
+}
 
 export default Verne;
+
+/* 
+  const caret: Caret = {
+    get offset() {
+      const selection = window.document.getSelection();
+      const range = selection?.getRangeAt(0);
+      return range?.startOffset;
+    },
+    get node() {
+      const selection = window.document.getSelection();
+      const range = selection?.getRangeAt(0);
+      return range?.startContainer;
+    },
+  }; */
